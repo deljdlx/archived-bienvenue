f8dd728985d8f54e1fd2c52f6018f108a6a11338
Jean luc <you@example.com>
2017-01-08 15:40:32 +0100

    synchro

---
+++ bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+//require(__DIR__.'/../../bootstrap.php');
+
+include(__DIR__.'/vendor/autoload.php');
+
+
+
+
+
+
+
+
+
+
+/*
+$dom=new \Phi\Module\Frontend\DOMTemplate();
+$template='
+	<html>
+		<div>
+			<p>test</p>
+			placeholder value : "<content select=".test"/>"
+		</div>
+	</html>
+';
+
+
+echo $dom->render($template, array(
+	'.test'=>'hello world'
+));
+*/
+
+
